7535beb1b22ebc6fb1a1ad5249099e57e708c0b7
manuelbocchia <zerounodos@gmail.com>
2025-04-05 02:59:57 -0300

    Get lyrics onto one file and transform them in format/clean broken words, and generate word map

--- lyric_gatherer.py
+++ lyric_gatherer.py
@@ -1,14 +1,18 @@
-import numpy as np
+#import numpy as np
 import pandas as pd
-import requests
-from bs4 import BeautifulSoup as bs
-from rauth import OAuth1Service
+#import requests
+#from bs4 import BeautifulSoup as bs
+#from rauth import OAuth1Service
 import pprint
-from collections import defaultdict
-import string
+#from collections import defaultdict
+#import string
 import json
 import os
 import re
+import enchant
+
+# Initialize the English dictionary for checking correct spelling of words
+d = enchant.Dict("en_US")
 
 artist = 'Queen'
 folder=f'LYRICS_{artist}'
@@ -16,7 +20,33 @@ list_of_songs = os.listdir(folder)
 
 all_the_words = ''
 
-#pprint.pp(list_of_songs)
+
+# Words to be removed so as to leave only content words, where possible
+structural_words = [
+    'A', 'AN', 'THE',  # Articles
+    'AND', 'BUT', 'OR', 'NOR', 'FOR', 'SO', 'YET',  # Conjunctions
+    'TO', 'IN', 'ON', 'AT', 'BY', 'WITH', 'ABOUT', 'AGAINST', 'BETWEEN', 'UNDER', 'OVER', 'THROUGH', 'DURING', 'BEFORE', 'AFTER',  # Prepositions
+    'OF', 'AS', 'FROM', 'UP', 'DOWN', 'FOR', 'INTO', 'ONTO', 'UPON', 'OUT', 'ALONG', 'AROUND', 'BEHIND', 'IN FRONT OF', 'OUTSIDE', 'INSIDE', 'WITHIN', 'WITHOUT',  # More Prepositions
+    'I', 'YOU', 'HE', 'SHE', 'IT', 'WE', 'THEY', 'ME', 'HIM', 'HER', 'US', 'THEM', 'MY', 'YOUR', 'HIS', 'HER', 'ITS', 'OUR', 'THEIR',  # Pronouns
+    'AM', 'IS', 'ARE', 'WAS', 'WERE', 'BE', 'BEEN', 'BEING',  # Forms of the verb 'to be'
+    'HAVE', 'HAS', 'HAD', 'HAVING',  # Forms of the verb 'to have'
+    'DO', 'DOES', 'DID', 'DOING',  # Forms of the verb 'to do'
+    'WILL', 'SHALL', 'WOULD', 'SHOULD', 'CAN', 'COULD', 'MAY', 'MIGHT', 'MUST', 'OUGHT', 'NEED', 'DARE',  # Modal verbs
+    'NOT', 'NO', 'YES', 'THIS', 'THAT', 'THESE', 'THOSE', 'THERE', 'HERE', 'WHERE', 'WHEN', 'WHY', 'HOW', 'ALL', 'SOME', 'ANY', 'EACH', 'EVERY', 'FEW', 'MANY', 'MUCH', 'MOST', 'MORE', 'LESS', 'LEAST',  # Determiners, adverbs, question words
+    'EITHER', 'NEITHER', 'BOTH', 'EACH', 'ANOTHER', 'SUCH', 'OTHER', 'ANOTHER',  # More determiners
+    'IF', 'UNLESS', 'ALTHOUGH', 'BECAUSE', 'SINCE', 'WHILE', 'BEFORE', 'AFTER', 'UNTIL', 'WHETHER', 'AS', 'AS LONG AS', 'EVEN THOUGH', 'SO THAT', 'IN CASE', 'UNLESS', 'PROVIDED THAT',  # Subordinating conjunctions
+    'EITHER', 'NEITHER',  # Correlative conjunctions
+    'THAN', 'AS', 'LIKE',  # Comparative conjunctions
+    'I\'M', 'YOU\'RE', 'HE\'S', 'SHE\'S', 'IT\'S', 'WE\'RE', 'THEY\'RE',  # Contractions of "to be"
+    'I\'VE', 'YOU\'VE', 'HE\'S', 'SHE\'S', 'IT\'S', 'WE\'VE', 'THEY\'VE',  # Contractions of "have"
+    'I\'D', 'YOU\'D', 'HE\'D', 'SHE\'D', 'IT\'D', 'WE\'D', 'THEY\'D',  # Contractions of "would" and "had"
+    'I\'LL', 'YOU\'LL', 'HE\'LL', 'SHE\'LL', 'IT\'LL', 'WE\'LL', 'THEY\'LL',  # Contractions of "will"
+    'ISN\'T', 'AREN\'T', 'WASN\'T', 'WEREN\'T', 'HASN\'T', 'HAVEN\'T', 'DON\'T', 'DOESN\'T', 'DIDN\'T', 'CAN\'T', 'COULDN\'T', 'MAYN\'T', 'MIGHTN\'T', 'MUSTN\'T', 'SHAN\'T', 'SHOULDN\'T',  # Negative contractions
+    'I\'VE GOT', 'YOU\'VE GOT', 'HE\'S GOT', 'SHE\'S GOT', 'IT\'S GOT', 'WE\'VE GOT', 'THEY\'VE GOT',  # Got contractions
+    'OH', 'OOH', 'YEAH','AH', 'HEY', "GONNA"
+]
+
+# read all song lyrics and get them on a variable, and split it into a list
 
 for song in list_of_songs:
     song_path = f'{folder}/{song}'
@@ -24,10 +54,29 @@ for song in list_of_songs:
         for line in file:
             all_the_words = all_the_words + ' ' + line
 
-#song_words = defaultdict(list)
-
 song_words = re.sub('[)(*,_-]*','',all_the_words).upper().split()
 
+# Create a DataFrame, then drop all structural words, count the occurrence of the words, then drop the words which are misspelt
+# and those words which are under 3 letters.
 
+song_df = pd.DataFrame(song_words, index=None, columns=['Words'])
 
-print(song_words)
+song_df.drop(song_df.query(f'Words in {structural_words}').index,inplace=True)
+
+song_df_count = song_df['Words'].value_counts()
+
+valid_words = [word for word in song_df_count.index if d.check(word) and len(word) > 2]
+
+## your dictionary to make the word map
+
+valid_word_count_dict = song_df_count[song_df_count.index.isin(valid_words)].to_dict()
+
+#pprint.pp(valid_word_count_dict.items())
+
+file_path = f'{folder}/Word_dictionary_{artist}.json'
+
+# Write the dictionary to the JSON file
+with open(file_path, 'w') as file:
+    json.dump(valid_word_count_dict, file)
+
+print(f"Dictionary saved to {file_path}")
--- word_map_1.py
+++ word_map_1.py
@@ -1,9 +1,129 @@
 from PIL import Image, ImageDraw, ImageFont
-from random import Random
+import random
+import json
+import os
+
+# Define the font directories
+font_dirs = ['/usr/share/fonts', '/usr/local/share/fonts', os.path.expanduser('~/.fonts')]
+
+# Find all .ttf and .otf files
+# List of Unicode-compliant fonts known to support a wide character set
+unicode_fonts = [
+    "DejaVu Sans",
+    "DejaVu Sans Mono",
+    "DejaVu Serif",
+    "DejaVu Serif Mono",
+    "Noto Sans",
+    "Noto Serif",
+    "Noto Sans CJK",
+    "Noto Sans Symbol",
+    "Noto Sans Emoji",
+    "Liberation Sans",
+    "Liberation Serif",
+    "Liberation Mono",
+    "Ubuntu",
+    "Ubuntu Mono",
+    "FreeSans",
+    "FreeSerif",
+    "FreeMono",
+    "Cantarell",
+    "Roboto",
+    "Roboto Mono",
+    "Roboto Slab",
+    "Source Sans Pro",
+    "Source Serif Pro",
+    "Source Code Pro",
+    "Symbola",
+    "M+ 1m",
+    "M+ 1p",
+    "M+ 2m",
+    "Arimo",
+    "STIX General",
+    "STIX Math",
+    "Luxi Sans",
+    "Luxi Serif",
+    "Luxi Mono",
+    "Tamsyn",
+    "Cairo",
+    "Zapfino",
+    "Bitstream Vera Sans",
+    "Bitstream Vera Serif",
+    "Bitstream Vera Mono"
+]
+
+# Find all .ttf and .otf files and filter those that are likely to support UTF-8
+fonts = []
+for font_dir in font_dirs:
+    for root, _, files in os.walk(font_dir):
+        for file in files:
+            if file.endswith('.ttf') or file.endswith('.otf'):
+                # Check if the font name matches a known Unicode font
+                if any(font in file for font in unicode_fonts):
+                    fonts.append(os.path.join(root, file))
+
+# Use a font
+#if fonts:
+#    font_path = fonts[0]  # Pick the first font from the list
+#    font = ImageFont.truetype(font_path, size=30)
+#    print(f"Using font: {font_path}")
+
+artist = 'Queen'
+folder=f'LYRICS_{artist}'
+# Specify the file path where the dictionary is saved
+file_path = f'{folder}/Word_dictionary_{artist}.json'
+
+# Read the dictionary from the JSON file
+with open(file_path, 'r') as file:
+    loaded_dict = json.load(file)
+
+print(f"Dictionary loaded: {len(loaded_dict)} words in total.")
+
 
 width, height = 1000, 1000
-big_image = Image.new('RGB', (width, height), 'white')
+big_image = Image.new('RGB', (width, height), 'black')
 draw = ImageDraw.Draw(big_image)
+
+
+##### CODE FOF VECTOR SEARCH ######
+occupied_pixels = [[False] * width for _ in range(height)]
+
+#def place_word(image, word, font, color, occupied_pixels):
+#    # Get word size (bounding box)
+#    word_width, word_height = ImageDraw.Draw(image).textsize(word, font=font)
+#    
+#    # Try placing the word until a valid position is found
+#    attempts = 0
+#    while attempts < 100:
+#        attempts += 1
+#        
+#        # Generate random position (x, y) for the word's top-left corner
+#        x = random.randint(0, image.width - word_width)
+#        y = random.randint(0, image.height - word_height)
+#        
+#        # Check if the word's area intersects with occupied pixels
+#        overlap = False
+#        for i in range(word_width):
+#            for j in range(word_height):
+#                if occupied_pixels[y + j][x + i]:
+#                    overlap = True
+#                    break
+#            if overlap:
+#                break
+#        
+#        if not overlap:
+#            # Draw the word on the image
+#            ImageDraw.Draw(image).text((x, y), word, font=font, fill=color)
+#            
+#            # Mark the pixels as occupied in the occupied_pixels matrix
+#            for i in range(word_width):
+#                for j in range(word_height):
+#                    occupied_pixels[y + j][x + i] = True
+#            
+#            return True  # Word placed successfully
+#    
+#    return False  # Failed to place the word after multiple attempts
+#
+####################################################################################
 
 
 def textsize(text, font):
@@ -15,16 +135,70 @@ def textsize(text, font):
 
 def put_word_into_text(text, size=50, rotation=0, font='LiberationSans-Regular.ttf', color='black', place=(0,0)):
     
+    #my_result = textsize(text, font)
+#
+    #word_width = my_result[0]
+    #word_height = my_result[1]
+
+
     font = ImageFont.truetype(font=font, size=size)
     text_image = Image.new('RGBA', textsize(text, font), (0,0,0,0))
     text_draw = ImageDraw.Draw(text_image)
     text_draw.text((0,0), text, font=font, fill=color)    
     rotated_text_image = text_image.rotate(rotation, expand=1)
-    big_image.paste(rotated_text_image, place, rotated_text_image)
 
-put_word_into_text('Hello Everybody!',55,20,place=(50,50))
-put_word_into_text('How are you doing?',40,45,place=(70,300))
-put_word_into_text('Is everything good?',30,90,place=(500,300))
-put_word_into_text('OH YEAH!',100,270,place=(900,500))   
+    word_width = round(text_draw.textlength(text, font))
+    word_height = size
+
+    x = random.randint(0, width - word_width)
+    y = random.randint(0, height - word_height)
+
+    #word_width, word_height = rotated_text_image.textsize(word, font=font)
+    #y = place[1] - word_height
+    #x = place[0] - word_width
+
+ 
+    # Try placing the word until a valid position is found
+    attempts = 0
+    while attempts < 100:
+        attempts += 1
+        overlap = False
+        for i in range(word_width):
+            for j in range(word_height):
+                if occupied_pixels[y + j][x + i]:
+                    overlap = True
+                    break
+            if overlap:
+                break
+            
+        if not overlap:
+            # Draw the word on the image
+            big_image.paste(rotated_text_image, (x,y), rotated_text_image)
+            
+            # Mark the pixels as occupied in the occupied_pixels matrix
+            for i in range(word_width):
+                for j in range(word_height):
+                    occupied_pixels[y + j][x + i] = True
+            
+            return True  # Word placed successfully
+
+        return False  # Failed to place the word after multiple attempts
+            
+
+for word, count in loaded_dict.items() :
+    #rand_locx=random.randrange(1000)
+    #rand_locy=random.randrange(1000)
+    #random_location = [rand_locx,rand_locy]
+    random_rotation = random.choice([0,90,180,270])
+    random_color = random.choice(['white','blue','green','red','yellow','purple'])
+    random_font = random.choice(fonts)
+    size = round(count/2) if count/5 > 1 else 1
+    if not put_word_into_text(word,size,random_rotation,font=random_font,color=random_color):
+        print(f"Failed to place word: {word}")
+
+#put_word_into_text('Hello Everybody!',55,20,place=(50,50))
+#put_word_into_text('How are you doing?',40,45,place=(70,300))
+#put_word_into_text('Is everything good?',30,90,place=(500,300))
+#put_word_into_text('OH YEAH!',100,270,place=(900,500))   
 
 big_image.show()
